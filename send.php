b082e61db14ddb55b3897a06924787671ee0f022
11Daniil <106248605+11Daniil@users.noreply.github.com>
2022-07-06 12:06:56 +0300

    Update send.php

--- send.php
+++ send.php
@@ -4,7 +4,7 @@ function validate($validate)
 	$requires = ['mail', 'phon', 'lastname', 'name'];
 	$phonValidate = preg_match("/^[8?\+7][0-9]{10}$/", $validate['phon']);
 	$errors = true;
-	
+
 	foreach($validate as $key => $val)
 	{
 		if(empty($validate[$key]) && in_array($key, $requires))
@@ -13,7 +13,8 @@ function validate($validate)
 	if(!filter_var($validate['mail'], FILTER_VALIDATE_EMAIL))
 		$errors = false;
 	if($errors === true){
-		// mail($to, $subject, $msg);
+		// mail($_POST['mail'], 'Тема письма', $_POST);
 	}
 }
+validate($_POST);
 ?>
